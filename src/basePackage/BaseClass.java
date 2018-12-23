f494820bd179418bd951c02ef0eef36fab9d180e
jr <jr@jr.com>
2018-12-23 20:49:15 +0100

    This is a next Step...

--- src/basePackage/BaseClass.java
+++ src/basePackage/BaseClass.java
@@ -5,8 +5,7 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("This is a sample...");
 		System.out.println("...next Step!..");
-		
-
+		System.out.println("...and a third Step!");
 	}
 
 }
